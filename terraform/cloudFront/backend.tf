6ab5bc38927d9e242b61f9a6ce6e932ef15c675f
DivHri <divya.aquila@gmail.com>
2025-08-21 17:27:36 -0500

    Update backend.tf

--- terraform/cloudFront/backend.tf
+++ terraform/cloudFront/backend.tf
@@ -1,10 +1,10 @@
 terraform { 
   cloud { 
     
-    organization = "sanskrit-gameshow" 
+    organization = "Sanskrit_Family_Feud_Gameshow" 
 
     workspaces { 
-      name = "gameshow-cloudFront" 
+      name = "gameshow-cloudfront" 
     } 
   } 
 }
